d367c2970d6b1550d4d5b25544c4fabac012efe6
float321 <187214682+float321@users.noreply.github.com>
2025-05-09 00:17:24 +0300

    Update favorites.py

--- db/models/favorites.py
+++ db/models/favorites.py
@@ -8,11 +8,13 @@ from sqlalchemy import orm
 class Favorites(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'favorites'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, 
+    id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
+    airport_departure = sqlalchemy.Column(sqlalchemy.String)
+    airport_arrival = sqlalchemy.Column(sqlalchemy.String)
+    flight_date = sqlalchemy.Column(sqlalchemy.String)
+    airline_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     flight_number = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    date = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
-    user_id = sqlalchemy.Column(sqlalchemy.Integer, 
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
