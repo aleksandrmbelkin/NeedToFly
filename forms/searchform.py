4f3b9176adf9eb29c5ab3e211d85b5d0f8de3a68
float321 <187214682+float321@users.noreply.github.com>
2025-05-09 00:16:40 +0300

    Update searchform.py

--- forms/searchform.py
+++ forms/searchform.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, PasswordField, StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField, validators
 from wtforms.validators import DataRequired
 
-        
+
 class SearchForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired(), validators.length(max=10)])
-    submit = SubmitField('Поиск')
+    airport = StringField('Название аэропорта (по системе IATA)',
+                          validators=[DataRequired(), validators.length(max=10)])
+    country = StringField('Название города (по системе IATA)',
+                          validators=[DataRequired(), validators.length(max=10)])
+    submit = SubmitField('Поиск', validators=[DataRequired()])
