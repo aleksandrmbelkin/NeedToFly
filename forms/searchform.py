a15ac6476f88088ce86458c7204d8797004ee61a
Александр <74454696+aleksandrmbelkin@users.noreply.github.com>
2025-05-15 18:43:42 +0300

    Update searchform.py

--- forms/searchform.py
+++ forms/searchform.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, validators
-from wtforms.validators import DataRequired
+from wtforms import validators, StringField, SubmitField, DateField, SelectField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, NumberRange
 
-
+        
 class SearchForm(FlaskForm):
-    airport = StringField('Название аэропорта (по системе IATA)',
-                          validators=[DataRequired(), validators.length(max=10)])
-    country = StringField('Название города (по системе IATA)',
-                          validators=[DataRequired(), validators.length(max=10)])
-    submit = SubmitField('Поиск', validators=[DataRequired()])
+    originLocationCode = StringField(validators=[DataRequired(), validators.length(max=30)])
+    destinationLocationCode = StringField(validators=[DataRequired(), validators.length(max=30)])
+
+    departureDate = DateField('Дата вылета', validators=[DataRequired()])
+    returnDate = DateField('Дата прилета', validators=[DataRequired()])
+
+    adults = IntegerField('Взрослые', validators=[DataRequired(), NumberRange(min=1, max=9)])
+    children = IntegerField('Дети', validators=[NumberRange(min=0, max=9)])
+    infants = IntegerField('Младенцы', validators=[NumberRange(min=0, max=9)])
+    
+    travelClass = SelectField('Выберите класс', choices=["Эконом", "Бизнесс"])
+    nonStop = BooleanField('Без пересадок')
+    submit = SubmitField('Поиск')
