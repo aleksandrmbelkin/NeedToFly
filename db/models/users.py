afaa482edbede42e6c355e4fa43c3341b66c3e67
float321 <187214682+float321@users.noreply.github.com>
2025-05-16 16:51:48 +0300

    Update users.py

--- db/models/users.py
+++ db/models/users.py
@@ -8,20 +8,22 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    # Создание таблицы users для БД
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, 
+    id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String, 
+    email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    favorites = orm.relationship("Favorites", back_populates='user') #user - колонка отношений в таблице
+    favorites = orm.relationship("Favorites", back_populates='user')  # user - колонка отношений в таблице
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
+    # Хэширование/проверка пароля
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
