1ce21fb06c409f422bbda5bc096b49e0410d0994
float321 <187214682+float321@users.noreply.github.com>
2025-05-16 16:50:24 +0300

    Update registerform.py

--- forms/registerform.py
+++ forms/registerform.py
@@ -4,8 +4,10 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
+    # Форма регистрации пользователя
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired(), validators.length(max=10)])
     submit = SubmitField('Войти')
+    
