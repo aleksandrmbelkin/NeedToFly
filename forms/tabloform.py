df536966ef7f09798b4f38950fa51ff0867b9c1f
float321 <187214682+float321@users.noreply.github.com>
2025-05-16 16:50:56 +0300

    Update tabloform.py

--- forms/tabloform.py
+++ forms/tabloform.py
@@ -4,8 +4,9 @@ from wtforms.validators import DataRequired
 
 
 class TabloForm(FlaskForm):
+    # Форма для поиска рейсов из конкретного аэропорта в другие страны/аэропорты
     airport = StringField('Название аэропорта (по системе IATA)',
-                          validators=[DataRequired(), validators.length(max=20)])
+                          validators=[DataRequired(), validators.length(max=10)])
     country = StringField('Название города (по системе IATA)',
-                          validators=[DataRequired(), validators.length(max=20)])
+                          validators=[DataRequired(), validators.length(max=10)])
     submit = SubmitField('Поиск', validators=[DataRequired()])
