73d9f90d8f49d947a34ab8fbae32f7b6fa9b1da4
float321 <187214682+float321@users.noreply.github.com>
2025-05-16 16:50:41 +0300

    Update searchform.py

--- forms/searchform.py
+++ forms/searchform.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import validators, StringField, SubmitField, DateField, SelectField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, NumberRange
 
-        
+
 class SearchForm(FlaskForm):
+    # Форма для поиска билетов
     originLocationCode = StringField(validators=[DataRequired(), validators.length(max=30)])
     destinationLocationCode = StringField(validators=[DataRequired(), validators.length(max=30)])
 
@@ -13,7 +14,7 @@ class SearchForm(FlaskForm):
     adults = IntegerField('Взрослые', validators=[DataRequired(), NumberRange(min=1, max=9)])
     children = IntegerField('Дети', validators=[NumberRange(min=0, max=9)])
     infants = IntegerField('Младенцы', validators=[NumberRange(min=0, max=9)])
-    
+
     travelClass = SelectField('Выберите класс', choices=["Эконом", "Бизнес"])
     nonStop = BooleanField('Без пересадок')
     submit = SubmitField('Поиск')
