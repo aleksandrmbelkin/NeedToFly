71dbf859732d3a6416628b34e9f68c39e45e19fc
float321 <187214682+float321@users.noreply.github.com>
2025-05-16 16:45:04 +0300

    Update tablo.py

--- pages/tablo.py
+++ pages/tablo.py
@@ -24,6 +24,7 @@ AVIATIONSTACK_KEY = os.getenv('AVIATIONSTACK_KEY')
 def search():
     form = TabloForm()
     if form.submit.data:
+        # Если пользователь подтвердил ввод, выводим при помощи amadeus информацию о рейсах
         amadeus = Client(client_id=AMADEUS_CLIENT_ID, client_secret=AMADEUS_CLIENT_SECRET)
         try:
             response = amadeus.airport.direct_destinations.get(departureAirportCode=str(form.airport.data))
@@ -32,22 +33,25 @@ def search():
                 lt.append({'country': i['address']['countryName'], 'airport_departure': str(form.airport.data),
                            'name': i["iataCode"]})
             return render_template('tablo.html', title='Поиск авиабилетов', header=True, form=form,
-                                   message='Code: 200', lt=lt)
+                                   message='', lt=lt)
         except Exception as e:
+            # Если возникла ошибка с amadeus, возвращаем ошибку
             print(e)
             return render_template('tablo.html', title='Поиск авиабилетов', header=True, form=form,
-                                   message='Code: 404')
+                                   message='Code 404')
     return render_template('tablo.html', title='Поиск авиабилетов', header=True, form=form)
 
 
 @blueprint.route('/<name>/<airport_departure>')
 def tickets(name, airport_departure):
+    # Возвращаем билеты
     lt = []
     no_ans = ''
     print(airport_departure, '->', name)
     url = f'https://api.aviationstack.com/v1/flights?access_key={AVIATIONSTACK_KEY}'
     response = requests.get(url, params={'dep_iata': airport_departure, 'arr_iata': name}).json()
     print(response)
+    # Создаём список словарей с информацией о билетах
     for i in response['data']:
         lt.append({"flight_date": i['departure']['scheduled'], 'airport_departure': i['departure']['airport'],
                    'airport_arrival': i['arrival']['airport'], "flight_date_arrival": i['arrival']['scheduled'],
